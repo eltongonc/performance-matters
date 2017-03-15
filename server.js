5a503b60dc2754d76037b14f948a4139a05e6098
Elton Goncalves <elton@XS2-MacBook-Pro.local>
2017-03-15 20:17:11 +0100

    added compression to the files via server.js file. Speed is 32s

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const nunjucks = require('nunjucks');
 const path = require('path');
 const routeStatic = require('./lib/route-static');
 const redirectIndices = require('./lib/redirect-indices');
+const compression = require('compression');
 
 const app = express();
 const baseDir = 'src';
@@ -11,9 +12,12 @@ const port = process.env.PORT || 3004;
 app.set('etag', false);
 app.use((req, res, next) => { res.removeHeader('X-Powered-By'); next(); });
 
+// compression
+app.use(compression());
+
 // static routes
 app.use(routeStatic);
-app.use('/static', express.static(path.join(__dirname, baseDir), { etag: false, lastModified: false }));
+app.use('/static', express.static(path.join(__dirname, baseDir), { etag: false, lastModified: false, maxage: '1y' }));
 
 // dynamic pages
 app.use(redirectIndices);
@@ -30,4 +34,3 @@ app.get('*', (req, res, next) => {
 app.listen(port, (err) => {
     err ? console.error(err) : console.log(`app running on http://localhost:${port}`);
 });
-
